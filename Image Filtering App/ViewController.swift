f8122d34e5f3a47885aa08f7d1d306831a194506
Timothy Barrett <tim.barrett.jr@gmail.com>
2017-04-07 17:57:46 -0700

    Working with cocoa pods

--- Image Filtering App/ViewController.swift
+++ Image Filtering App/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import Sharaku
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, SHViewControllerDelegate {
 
+    @IBOutlet weak var editedImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func openFilterButtonTapped(_ sender: UIButton) {
+        
+        let imageToBeFiltered = #imageLiteral(resourceName: "iceland")
+        
+        let vc = SHViewController(image: imageToBeFiltered)
+        vc.delegate = self
+        self.present(vc, animated: true)
+        
+    }
+    
+    func shViewControllerImageDidFilter(image: UIImage) {
+        
+        editedImageView.image = image
+        
+    }
+    
+    func shViewControllerDidCancel() {
+        
+        editedImageView.image = #imageLiteral(resourceName: "iceland")
+        dismiss(animated: true)
+    }
 }
 
